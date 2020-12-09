10c1b59b816c0cebcffc3e641ceea0ebfdda86b6
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-12-09 02:25:47 -0600

    removed unnecessary files, created simple grid in app.js

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import produce from 'immer'
+
+const numRows = 50
+const numCols = 50
 
 function App() {
+  const [grid, setGrid] = useState(() =>{
+    const rows = []
+    for (let i = 0; i < numRows; i++){
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+
+    return rows
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}>
+      {grid.map((rows, i) => 
+        rows.map((col, k) => ( 
+        <div 
+          key={`${i}-${k}`}
+          onClick={() => {
+
+          }}
+          style={{width:20, 
+            height: 20, 
+            backgroundColor: grid[i][k] ? 'pink' : undefined,
+            border: '1px solid black'
+          }}
+          />
+          ))
+      )}
     </div>
   );
 }
