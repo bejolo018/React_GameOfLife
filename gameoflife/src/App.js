47e4121c82e40eae24338720e0855b21dcb6465d
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-12-11 18:08:19 -0600

    added operations for cleaner code, using useRef hook for simple reference, created logic to check all the neighbors of a cell

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,8 +1,20 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import produce from 'immer'
 
 const numRows = 50
 const numCols = 50
+
+// Represents rows around grid cell
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+]
 
 function App() {
   const [grid, setGrid] = useState(() =>{
@@ -15,9 +27,36 @@ function App() {
   });
 
   const [running, setRunning] = useState(false)
+  
+  const runningRef = useRef(running);
+  runningRef.current = running
 
   const runSimulation = useCallback(() => {
+    if(!runningRef.current){
+      return;
+    }
 
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for(let i = 0; i < numRows; i++){
+          for (let j = 0; j < numCols; j++){
+            let neighbors = 0;
+            operations.forEach(([x, y]) =>{
+              const newI = i + x;
+              const newJ = j + y;
+              if(newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols){
+                neighbors += g[newI][newJ]
+              }
+            })
+
+            
+          }
+        }
+      });
+    });
+    const newGrid = produce()
+
+    setTimeout(runSimulation, 1000)
   }, [])
 
   return (
