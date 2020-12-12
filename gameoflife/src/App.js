0dc743bc4181dda0f9b5839af43b649a036ab5cd
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-12-11 18:16:03 -0600

    finished logic, added notes for clairification and connected it to my button, everythings working!

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -36,32 +36,45 @@ function App() {
       return;
     }
 
+    // Simulation
     setGrid((g) => {
       return produce(g, gridCopy => {
         for(let i = 0; i < numRows; i++){
           for (let j = 0; j < numCols; j++){
+            // Compute number of neighbors
             let neighbors = 0;
             operations.forEach(([x, y]) =>{
               const newI = i + x;
               const newJ = j + y;
+            // ensures we stay within in the grid
               if(newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols){
                 neighbors += g[newI][newJ]
               }
             })
 
-            
+            // Determines whether a cell lives or dies
+            if(neighbors < 2 || neighbors > 3){
+              gridCopy[i][j] = 0
+            } else if(g[i][j] === 0 && neighbors ===3){
+              gridCopy[i][j] = 1;
+            }
           }
         }
       });
     });
-    const newGrid = produce()
 
-    setTimeout(runSimulation, 1000)
+    setTimeout(runSimulation, 100)
   }, [])
 
   return (
     <>
-      <button onClick={() => {setRunning(!running)}}>{running ? 'stop' : 'start'}</button>
+      <button onClick={() => {
+        if(!running){
+        setRunning(!running)
+        runningRef.current = true;
+        runSimulation()
+        }
+      }}>{running ? 'stop' : 'start'}</button>
 
       <div style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}>
         {grid.map((rows, i) => 
